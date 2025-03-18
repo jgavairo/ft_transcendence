85d3cabb41d009b768458971f1435ac27404d391
Jordan Gavairon <jgavairo@z1r3p1.42lyon.fr>
2025-03-18 15:18:48 +0100

    efesggr

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+const fastify = require('fastify')({ logger: true });
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000, host: '0.0.0.0' });
+    fastify.log.info(`Server listening on ${fastify.server.address().port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
