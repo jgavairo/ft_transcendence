81f31435ff13446ead08448686ea9c69344cb7a6
Axel Bolea <boleaaxel@gmail.com>
2025-03-26 13:14:33 +0100

    library

--- scripts/sourcepage.ts
+++ scripts/sourcepage.ts
@@ -26,30 +26,32 @@ const storePage = `
 `;
 
 const libraryPage = `
-      <div class="flex flex-1">
-      <aside class="w-1/4 bg-[#24282F] overflow-y-auto p-4">
-        <div class="mb-4 relative">
-          <input 
+    <div class="container">
+    <aside class="sidebar">
+      <div class="search-container flex">
+        <input 
           type="text" 
           placeholder="Search" 
-          class="w-full pl-8 pr-2 py-2 rounded bg-gray-700 placeholder-gray-400 text-white">
-          <div class="absolute inset-y-0 left-0 flex items-center pl-2 pointer-events-none">
-            <svg class="h-4 w-4 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-4.35-4.35M9.5 17a7.5 7.5 0 100-15 7.5 7.5 0 000 15z"/>
-            </svg>
-          </div>
+          class="search-input">
+        <div class="search-icon">
+          <svg class="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+              d="M21 21l-4.35-4.35M9.5 17a7.5 7.5 0 100-15 7.5 7.5 0 000 15z"/>
+          </svg>
         </div>
-          <ul class="library-games-list"></ul>
-        </aside>
-        <section class="w-full">
-          <div class="library-details">
-          <div class="flex items-center mb-4">
-             <h2 class="mt-2 text-xl font-bold text-[#8F8F8F]">All games (0)</h2>
-          <div class="mt-2 flex-1 border-t border-gray-500 ml-4"></div>
-          </div>
-          </div>
-        </section>
       </div>
+      <ul class="library-games-list"></ul>
+    </aside>
+
+    <section class="main-section">
+      <div class="library-details">
+        <div class="header-section flex">
+          <h2>All games (0)</h2>
+          <div class="divider"></div>
+        </div>
+      </div>
+    </section>
+  </div>
 `;
 
 const communityPage = `
