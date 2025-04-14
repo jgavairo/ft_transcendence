88bee55f5879423d4f4c420c24b84cba75f8b1e4
Jordan Gavairon <jgavairo@z1r4p3.42lyon.fr>
2025-04-14 16:24:41 +0200

    style for profile customisation (hover profile picture) added

--- scripts/sourcepage.js
+++ scripts/sourcepage.js
@@ -72,7 +72,12 @@ const profileModalHTML = (username, email, profilePicture) => `
           <div class="profile-modal-content">
             <button class="closeProfileModal" id="closeProfileModal">X</button>
             <h2>Profile settings</h2>
-            <img src="${profilePicture}" class="pictureProfileModal" alt="Profile Picture" />
+            <div class="profile-picture-container">
+              <img src="${profilePicture}" class="pictureProfileModal" alt="Profile Picture" />
+              <div class="profile-picture-overlay">
+                Edit
+              </div>
+            </div>
             <div class="profile-modal-content-info">
               <div id="username">
                 <h3>Username: ${username}</h3>
