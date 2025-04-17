1a4723d55cae70152763debb73d1b5ad70f3b1ec
Axel Bolea <boleaaxel@gmail.com>
2025-04-17 11:49:19 +0200

    pong moving

--- backend/src/games/pong/gameSimulation.ts
+++ backend/src/games/pong/gameSimulation.ts
@@ -1,3 +1,4 @@
+
 export interface MatchState {
   roomId: string;
   leftPaddleY: number;
@@ -15,7 +16,7 @@ export interface MatchState {
   gameOver?: boolean;
 }
 
-const TICK_RATE = 45; // FPS
+const TICK_RATE = 30; // FPS
 const CANVAS_WIDTH = 1200;
 const CANVAS_HEIGHT = 800;
 const BALL_RADIUS = 10;
