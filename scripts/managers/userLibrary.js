106b7de5fab60c89a13d77b2578a340005da1da3
Axel Bolea <boleaaxel@gmail.com>
2025-04-26 14:01:48 +0200

    code clean

--- scripts/managers/userLibrary.js
+++ scripts/managers/userLibrary.js
@@ -1,12 +1,3 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 import api from '../helpers/api.js';
 export class UserLibraryManager {
     static getCurrentUser() {
@@ -31,43 +22,37 @@ export class UserLibraryManager {
         localStorage.setItem(this.STORAGE_KEY, JSON.stringify(newLibrary));
         return newLibrary;
     }
-    static hasGame(gameId) {
-        return __awaiter(this, void 0, void 0, function* () {
-            console.log("Checking if game is in library:", gameId);
-            const response = yield api.get('http://127.0.0.1:3000/api/user/library');
-            console.log("API response:", response);
-            const data = yield response.json();
-            console.log("API data:", data);
-            if (data.success) {
-                if (data.library.includes(gameId))
-                    return true;
-            }
-            return false;
-        });
+    static async hasGame(gameId) {
+        console.log("Checking if game is in library:", gameId);
+        const response = await api.get('http://127.0.0.1:3000/api/user/library');
+        console.log("API response:", response);
+        const data = await response.json();
+        console.log("API data:", data);
+        if (data.success) {
+            if (data.library.includes(gameId))
+                return true;
+        }
+        return false;
     }
-    static addGame(gameId) {
-        return __awaiter(this, void 0, void 0, function* () {
-            console.log("Adding game to library:", gameId);
-            const response = yield api.post('http://127.0.0.1:3000/api/user/addGame', { gameId });
-            console.log("API response:", response);
-            const data = yield response.json();
-            if (data.success) {
-                console.log("Game added to library:", gameId);
-            }
-            else {
-                console.log("Game not added to library:", gameId);
-            }
-        });
+    static async addGame(gameId) {
+        console.log("Adding game to library:", gameId);
+        const response = await api.post('http://127.0.0.1:3000/api/user/addGame', { gameId });
+        console.log("API response:", response);
+        const data = await response.json();
+        if (data.success) {
+            console.log("Game added to library:", gameId);
+        }
+        else {
+            console.log("Game not added to library:", gameId);
+        }
     }
-    static getLibraryGames() {
-        return __awaiter(this, void 0, void 0, function* () {
-            const response = yield api.get('http://127.0.0.1:3000/api/user/library');
-            const data = yield response.json();
-            if (data.success)
-                return data.library;
-            else
-                return [];
-        });
+    static async getLibraryGames() {
+        const response = await api.get('http://127.0.0.1:3000/api/user/library');
+        const data = await response.json();
+        if (data.success)
+            return data.library;
+        else
+            return [];
     }
 }
 UserLibraryManager.STORAGE_KEY = 'userLibrary';
---
+++ scripts/pages/library/showGameDetails.ts
@@ -0,0 +1,79 @@
+import { fetchUsernames } from "../community/peopleList.js";
+import { gameModalHTML } from "../../sourcepage.js";
+import { displayMenu } from '../../games/pong/pongGame.js';
+import { GameManager } from "../../managers/gameManager.js";
+import { setupLibrary } from "./library.js";
+
+export async function showGameDetails(gameIdOrObj: number | any): Promise<void> {
+  // Récupérer l'objet game complet
+  let game;
+  if (typeof gameIdOrObj === 'number') {
+    const allGames = await GameManager.getGameList();
+    game = allGames.find(g => g.id === gameIdOrObj);
+  } else {
+    game = gameIdOrObj;
+  }
+  if (!game) return;
+
+  // Récupérer les utilisateurs
+  const people = await fetchUsernames();
+
+  const details = document.querySelector('.library-details') as HTMLElement;
+  if (!details) return;
+
+  details.innerHTML = `
+    <div class="detail-container">
+      <div class="detail-image">
+        <button class="close-button">&times;</button>
+        <img src="${game.image}" alt="${game.name}">
+        <div class="bannerGameSelect">
+          <button id="launchGameButton" class="playButton">PLAY</button>
+        </div>
+      </div>
+      <div class="detail-info">
+        <div class="rankingContainer">
+          <h3 class="sectionTitle">Player Ranking</h3>
+          <ul class="rankingList">
+            <li class="rankingItem">
+              <span class="numberRank">1</span>
+              <img src="/assets/pp.png" class="profilePic" alt="Profile">
+              <div class="playerInfo">
+                <span class="playerName">Jordan</span>
+                <span class="playerWins">Wins: 10</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="friendsContainer">
+          <h3 class="sectionTitle">People List</h3>
+          <ul class="friendsList">
+            ${people.map(person => `
+              <li class="friendItem">
+                <img src="${person.profile_picture || 'default-profile.png'}" class="profilePic" alt="${person.username}">
+                <span class="friendName">${person.username}</span>
+              </li>
+            `).join('')}
+          </ul>
+        </div>
+      </div>
+    </div>
+  `;
+
+  // Bouton de fermeture
+  const closeBtn = details.querySelector('.close-button') as HTMLButtonElement;
+  closeBtn.addEventListener('click', () => {
+    setupLibrary();
+  });
+
+  // Bouton PLAY
+  const playBtn = details.querySelector('#launchGameButton') as HTMLButtonElement;
+  playBtn.addEventListener('click', () => {
+    const modal = document.getElementById('optionnalModal');
+    if (!modal) return;
+    modal.innerHTML = gameModalHTML;
+    displayMenu();
+    window.addEventListener('keydown', e => {
+      if (e.key === 'Escape') modal.innerHTML = '';
+    }, { once: true });
+  });
+}
