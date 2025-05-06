533cfff9098051984e369970238fce37c0c8158c
haurujenkins <loic.lpr@gmail.com>
2025-05-06 18:57:53 +0200

    games descrptions updated

--- backend/src/database/database.ts
+++ backend/src/database/database.ts
@@ -69,21 +69,21 @@ export class DatabaseManager
                         id: 1,
                         name: "Pong",
                         price: 0,
-                        description: "Un jeu classique de tennis de table en 2D.",
+                        description: "Pong is a two-dimensional sports game that simulates table tennis. The player controls an in-game paddle by moving it vertically along the left or right side of the screen, and can use the paddle to hit a ball back and forth across the screen. The goal is to manoeuvre the ball past the opponent's paddle and into the opposing side's court, and to prevent the ball from being hit back into their own court",
                         image: "../../assets/games/pong/pong.png",
                     },
                     {
                         id: 3,
                         name: "Valorant",
                         price: 29.99,
-                        description: "Un FPS compétitif avec des héros uniques.",
+                        description: "Une pale copie de Counter-Strike, mais avec des personnages de la série Rick and Morty",
                         image: "../../assets/games/valorant/valorant.png",
                     },
                     {
                         id: 2,
                         name: "Francis the Loony",
                         price: 299.99,
-                        description: "Survivez à une soirée étrange chez Francis.",
+                        description: "Recevez une invitation mystérieuse à la soirée de Francis le Tordu (YOUPiiIIII). Dans ce jeu psychologique troublant, naviguez à travers une demeure victorienne où les règles changent constamment et où vos choix ont des conséquences... imprévisibles. Les autres invités semblent connaître les règles, mais personne ne veut les partager. Évitez les regards tordus de Francis, découvrez pourquoi certains invités disparaissent après avoir bu le cocktail du chef, et tentez de survivre jusqu'au matin. Mais attention : Francis déteste quand on refuse de danser la valse inversée",
                         image: "../../assets/games/snake/snake.png",
                     }
                 ];
